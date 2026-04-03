3a0c199594e80d38b12d730de8b674a498ea0ccc
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-03 15:26:24 +0300

    Kullanici.cs

--- Models/Kullanici.cs
+++ Models/Kullanici.cs
@@ -3,7 +3,9 @@ namespace MuhasebeTakip2.App.Models
     public class Kullanici
     {
         public int Id { get; set; }
+
         public string KullaniciAdi { get; set; } = "";
+
         public string Sifre { get; set; } = "";
 
         public int FirmaId { get; set; }
