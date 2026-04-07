f41a7afad5cff5391ea46c0de8406dd50afc85c0
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-07 17:04:27 +0300

    Fix kasa page null-safe rendering

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec7ac1221df0e85eeae44f91c8cf4c2aeecf698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93bc68a1f473c9c1d759217e5096ddfafaa3290")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
