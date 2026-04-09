6fd203d365d2b0e169cfab6afb862556f903254d
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-09 16:14:43 +0300

    Musteriler sayfasi duzeltildi

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126aeb6cde37472da5fb3734bab8b3d24f957cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4539e4b01f6bdb7834ad93ad73c5fe250890b800")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
