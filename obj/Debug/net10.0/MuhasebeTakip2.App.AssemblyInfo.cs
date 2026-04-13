8fc7d8261b63f8006e8d67a696332eae087c3bfd
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-13 16:47:31 +0300

    Puantajda gelecek gunler bos gosterildi

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e50889fa65cc7657296e5eb274f84004b6e9a96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2a8a64de51c377537889765f25ed3406ac0635")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
