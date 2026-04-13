f93088ba6070a50a4314d785f8245d6369c19b89
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-13 17:30:14 +0300

    Puantaj gelecek tarihler bos gosterilecek sekilde duzeltildi

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc7d8261b63f8006e8d67a696332eae087c3bfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c0dc7814c9ee93622c8a1919730019c87f7f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
