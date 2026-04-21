3f746a1d6f89718f8e68a142d3964e2f635bac5f
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-04-21 11:48:57 +0300

    düzenleme

--- FirmovaAI/Models/Muhasebe/Calisan.cs
+++
@@ -1,8 +0,0 @@
-namespace FirmovaAI.Models.Muhasebe;
-
-public class Calisan
-{
-    public int Id { get; set; }
-    public string AdSoyad { get; set; } = "";
-    public int? FirmaId { get; set; }
-}
--- FirmovaAI/Models/Muhasebe/CalisanHareketTipi.cs
+++
@@ -1,7 +0,0 @@
-namespace FirmovaAI.Models.Muhasebe;
-
-public enum CalisanHareketTipi
-{
-    Avans = 1,
-    MaasOdeme = 2
-}
