499afeb0c6c9ae25c291e432002150809d09c835
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-05-02 17:02:55 +0300

    deneme

--- FirmovaAI/obj/Debug/net10.0/FirmovaAI.AssemblyInfo.cs
+++ FirmovaAI/obj/Debug/net10.0/FirmovaAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirmovaAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f92b54dc9e5537298514fedd35b0903c8d6c3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba5b5057f9b960074fe7fe33edb8821fb5e5724")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirmovaAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirmovaAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
