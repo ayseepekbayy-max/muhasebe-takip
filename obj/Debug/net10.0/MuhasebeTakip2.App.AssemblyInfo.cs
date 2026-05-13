8da3270b6ba7fe5daa2436bc128d7760e5bd93de
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-05-13 11:07:35 +0300

    mobil nova duzeltildi

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb100341edf0b3ce7a73e6548acefab90b369854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9864bdee6a81d33bae37d814e1a38d370fcbe396")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
