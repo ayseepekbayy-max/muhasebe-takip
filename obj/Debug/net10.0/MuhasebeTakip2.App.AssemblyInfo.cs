3d6bd1fbc3df069feb802c9d04d6179a44c9a97d
ayseepekbayy-max <ayseepekbayy@gmail.com>
2026-05-15 11:47:12 +0300

    bbb

--- obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
+++ obj/Debug/net10.0/MuhasebeTakip2.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94df7b063907b115d9c4a47388a57878a28bc95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e723608d9bc2a2fffb2785d36b5a783ae9ce80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuhasebeTakip2.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
